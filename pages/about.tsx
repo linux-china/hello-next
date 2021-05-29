b4dabbfedc1851a3ad9d5fefaafcb339303ae650
linux_china <libing.chen@gmail.com>
2021-05-29 13:47:05 -0700

    Add dynamic data fetch

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,58 @@
-function About() {
-    return <div>About</div>
-}
+import {useEffect, useState} from 'react';
 
-export default About;
+import axios from 'axios';
+
+const Users = () => {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const fetchData = async () => {
+        await axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(res => {
+                setError(false);
+                setUsers(prevState => [...prevState, ...res.data]);
+            })
+            .catch(() => {
+                setError(true);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+    };
+
+    useEffect(() => {
+        fetchData().then();
+    }, []);
+
+    return (
+        <section>
+            <header>
+                <h1>List of users</h1>
+            </header>
+            {!error && loading && <div>Loading data...</div>}
+            {error && <div>There was an error.</div>}
+            {!error && users && (
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Username</th>
+                        <th>Email</th>
+                        <th>Name</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {users.map((user, key) => (
+                        <tr key={key}>
+                            <td>{user.username}</td>
+                            <td>{user.email}</td>
+                            <td>{user.name}</td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            )}
+        </section>
+    );
+};
+
+export default Users;
